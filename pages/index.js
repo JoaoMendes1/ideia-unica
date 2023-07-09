a6b2087075fe7f6602762f0f2523cb9f20150c96
João Victor Mendes <50036673+JoaoMendes1@users.noreply.github.com>
2023-07-08 23:22:22 -0500

    Primeira página

---
+++ pages/index.js
@@ -0,0 +1,36 @@
+import { useState, userState } from "react";
+
+function Home() {
+    return (
+        <div>
+            <h2>Home</h2>
+            <Contador/>
+            <div>Teste</div>
+        </div>
+    )
+}
+
+function Contador() {
+    const [contador,setContador] = useState(1);
+
+    function adicionarContador() {
+        setContador(contador + 1);
+    }
+
+    function removerContador() {
+        setContador(contador - 1);
+    }
+
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+            <button onClick={removerContador}>Remover</button>
+        </div>
+    )
+}
+
+
+
+
+export default Home
